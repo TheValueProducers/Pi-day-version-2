eb1597b18e9cc04868e9aa075d40c97a1340e909
TheValueProducers <145182357+TheValueProducers@users.noreply.github.com>
2025-02-17 21:14:17 +0700

    added time clock

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,9 +1,10 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import './App.css';
+
 import Test from "./pages/Test"
-import TestInstruction from "./pages/TestInstruction";
 import CreateAccount from "./pages/CreateAccount";
 import ManageAccount from "./pages/ManageAccount";
+import TestInstruction from "./pages/TestInstruction";
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
         <Route exact path = "/student/test" element = {<Test />} />
         <Route exact path = "/admin/manage-account" element = {<ManageAccount />}/>
         <Route exact path = "/admin/create-account" element = {<CreateAccount />}  />
+
+
       </Routes>
     </Router>
   );
