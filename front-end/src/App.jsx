940e64287c31b185ff454080a4cec506c909d2ea
Ezylynn <110110711+Ezylynn@users.noreply.github.com>
2025-02-28 23:23:40 +0700

    merge with header-footer-tweak

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -12,19 +12,17 @@ import NavBarTester from "./pages/NavBarTester"
 function App() {
   return (
 
-    <NavBarTester/>
-
-    // <Router>
-    //   <Routes>
-    //     <Route exact path = "/student/register" element = {<Register/>} />
-    //     <Route exact path = "student/sign-in" element = {<UserAuth />}/>
-    //     <Route exact path = "/student/test-instruction" element = {<TestInstruction />} />
-    //     <Route exact path = "/student/test" element = {<Test />} />
-    //     <Route exact path = "/admin/manage-account" element = {<ManageAccount />}/>
-    //     <Route exact path = "/admin/create-account" element = {<CreateAccount />}  />
-    //     <Route exact path = "/admin/sign-in" element = {<AdminAuth />} />
-    //   </Routes>
-    // </Router>
+    <Router>
+      <Routes>
+        <Route exact path = "/student/register" element = {<Register/>} />
+        <Route exact path = "student/sign-in" element = {<UserAuth />}/>
+        <Route exact path = "/student/test-instruction" element = {<TestInstruction />} />
+        <Route exact path = "/student/test" element = {<Test />} />
+        <Route exact path = "/admin/manage-account" element = {<ManageAccount />}/>
+        <Route exact path = "/admin/create-account" element = {<CreateAccount />}  />
+        <Route exact path = "/admin/sign-in" element = {<AdminAuth />} />
+      </Routes>
+    </Router>
   );
 }
 
