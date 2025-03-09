5bd3006d17be2a46321c88faa5808019529de03b
TheValueProducers <145182357+TheValueProducers@users.noreply.github.com>
2025-03-09 14:44:38 +0700

    changed bcrypt to bcryptjs

--- back-end/controllers/AdminController.js
+++ back-end/controllers/AdminController.js
@@ -1,6 +1,6 @@
 require("dotenv").config()
 const {Admin, Teacher, Attempt, Game} = require('../models');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken")
 const JWT_SECRET = process.env.JWT_SECRET
 
