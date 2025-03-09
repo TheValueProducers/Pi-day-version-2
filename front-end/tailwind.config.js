67296790f53d2710e43a062cccf8c775141643a0
TheValueProducers <145182357+TheValueProducers@users.noreply.github.com>
2025-03-09 18:29:56 +0700

    undoing changes

--- front-end/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // Ensure all relevant files are included
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
