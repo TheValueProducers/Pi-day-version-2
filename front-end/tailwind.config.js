da0c10c5e2425ecffefa9409d1c137b7b8a9465d
TheValueProducers <145182357+TheValueProducers@users.noreply.github.com>
2025-03-09 18:19:50 +0700

    added postcss.config

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,4 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-    // Other Tailwind configurations
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // Ensure all relevant files are included
+    theme: {
+      extend: {},
+    },
+    plugins: [],
   };
